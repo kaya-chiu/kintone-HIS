ef1fe410d22d9df1153618dddce322e61254b56c
kaya-chiu <tzuhsuan-chiu@cybozu.co.jp>
2024-06-05 14:28:13 +0800

    fix: order of kintone events

--- 01病患資料管理/src/main.tsx
+++ 01病患資料管理/src/main.tsx
@@ -9,22 +9,7 @@ import './main.css'
 import DetailTab from './components/DetailTab'
 
 // *** Events ***
-kintone.events.on(config.events.all.show, event => {
-  disabledField(['病歷號碼', '年齡', '預欠收_台幣', '預欠收_美金'], event)
-  return event
-})
-
-kintone.events.on(config.events.create.show, autoId)
-
-kintone.events.on([
-  ...config.events.all.show,
-  ...config.events.show,
-  `app.record.create.change.${config.fc.patient.生日}`,
-  `app.record.edit.change.${config.fc.patient.生日}`,
-  `mobile.app.record.create.change.${config.fc.patient.生日}`,
-  `mobile.app.record.edit.change.${config.fc.patient.生日}`,
-], showAge)
-
+// 分頁 Tab (要放在最上面，避免覆蓋其他事件)
 kintone.events.on([
   'app.record.detail.show',
   'app.record.create.show',
@@ -33,3 +18,22 @@ kintone.events.on([
   const el = kintone.app.record.getSpaceElement('tab')
   ReactDOM.createRoot(el!).render(<DetailTab event={event} />)
 })
+
+// 禁止編輯欄位
+kintone.events.on(config.events.all.show, event => {
+  disabledField(['病歷號碼', '年齡', '預欠收_台幣', '預欠收_美金'], event)
+  return event
+})
+
+// 病歷號碼自動編碼
+kintone.events.on(config.events.create.show, autoId)
+
+// 自動計算當前年齡
+kintone.events.on([
+  ...config.events.all.show,
+  ...config.events.show,
+  `app.record.create.change.${config.fc.patient.生日}`,
+  `app.record.edit.change.${config.fc.patient.生日}`,
+  `mobile.app.record.create.change.${config.fc.patient.生日}`,
+  `mobile.app.record.edit.change.${config.fc.patient.生日}`,
+], showAge)
