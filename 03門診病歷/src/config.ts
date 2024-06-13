c5bcde963611e9f08b590e5462dcf91456c8c10d
kaya-chiu <tzuhsuan-chiu@cybozu.co.jp>
2024-06-13 10:41:37 +0800

    project init: 03 OPD

---
+++ 03門診病歷/src/config.ts
@@ -0,0 +1,163 @@
+import { KintoneTypes } from './dts/types'
+
+interface Editable {
+  show: string[],
+  submit: string[],
+  success: string[]
+}
+interface Config {
+  events: {
+    create: Editable,
+    edit: Editable,
+    all: Editable,
+    show: string[]
+  },
+  fc: {
+    opd: KintoneTypes.Fc<keyof KintoneTypes.Opd>
+  },
+  // sp: {
+  //   search: string
+  // },
+  // el: {
+  //   spacer: string
+  // }
+}
+
+const config: Config = {
+  events: {
+    create: {
+      show: [
+        'app.record.create.show',
+        'mobile.app.record.create.show',
+      ],
+      submit: [
+        'app.record.create.submit',
+        'mobile.app.record.create.submit',
+      ],
+      success: [
+        'app.record.create.submit.success',
+        'mobile.app.record.create.submit.success',
+      ]
+    },
+    edit: {
+      show: [
+        'app.record.edit.show',
+        'app.record.index.edit.show',
+        'mobile.app.record.edit.show',
+      ],
+      submit: [
+        'app.record.edit.submit',
+        'app.record.index.edit.submit',
+        'mobile.app.record.edit.submit',
+      ],
+      success: [
+        'app.record.edit.submit.success',
+        'app.record.index.edit.submit.success',
+        'mobile.app.record.edit.submit.success',
+      ]
+    },
+    all: {
+      show: [
+        'app.record.create.show',
+        'mobile.app.record.create.show',
+        'app.record.edit.show',
+        'app.record.index.edit.show',
+        'mobile.app.record.edit.show',
+      ],
+      submit: [
+        'app.record.create.submit',
+        'mobile.app.record.create.submit',
+        'app.record.edit.submit',
+        'app.record.index.edit.submit',
+        'mobile.app.record.edit.submit',
+      ],
+      success: [
+        'app.record.create.submit.success',
+        'mobile.app.record.create.submit.success',
+        'app.record.edit.submit.success',
+        'app.record.index.edit.submit.success',
+        'mobile.app.record.edit.submit.success',
+      ]
+    },
+    show: [
+      'app.record.detail.show',
+      'mobile.app.detail.show',
+      'app.record.index.show',
+      'mobile.app.record.index.show',
+    ]
+  },
+  fc: {
+    opd: {
+      $id: '$id',
+      $revision: '$revision',
+      更新人: '更新人',
+      建立人: '建立人',
+      記錄號碼: '記錄號碼',
+      更新時間: '更新時間',
+      建立時間: '建立時間',
+      預設劑量: '預設劑量',
+      分管組合搜尋: '分管組合搜尋',
+      檢驗價格3: '檢驗價格3',
+      姓名: '姓名',
+      檢驗價格2: '檢驗價格2',
+      檢驗價格1: '檢驗價格1',
+      身份別: '身份別',
+      實收: '實收',
+      藥品名稱搜尋: '藥品名稱搜尋',
+      藥品價格3: '藥品價格3',
+      病歷號碼: '病歷號碼',
+      批價代碼: '批價代碼',
+      藥品價格1: '藥品價格1',
+      藥品價格2: '藥品價格2',
+      生日: '生日',
+      批價狀態: '批價狀態',
+      預設頻率: '預設頻率',
+      藥品代碼搜尋: '藥品代碼搜尋',
+      Objective: 'Objective',
+      檢驗名稱搜尋: '檢驗名稱搜尋',
+      批價幣別: '批價幣別',
+      就診日期: '就診日期',
+      掛號費_台幣: '掛號費_台幣',
+      已領藥: '已領藥',
+      看診序號: '看診序號',
+      掛號流水號: '掛號流水號',
+      性別: '性別',
+      處置名稱搜尋: '處置名稱搜尋',
+      檢驗代碼搜尋: '檢驗代碼搜尋',
+      門診時段: '門診時段',
+      門診別: '門診別',
+      Subjective: 'Subjective',
+      預欠收: '預欠收',
+      處置價格1: '處置價格1',
+      分區: '分區',
+      處置價格3: '處置價格3',
+      就診類別: '就診類別',
+      處置價格2: '處置價格2',
+      處置代碼搜尋: '處置代碼搜尋',
+      療程別: '療程別',
+      病歷記錄流水號: '病歷記錄流水號',
+      身份證號: '身份證號',
+      掛號費_美金: '掛號費_美金',
+      預設天數: '預設天數',
+      應收: '應收',
+      掛號費: '掛號費',
+      對應藥品價格: '對應藥品價格',
+      處置費: '處置費',
+      對應檢驗價格: '對應檢驗價格',
+      對應處置價格: '對應處置價格',
+      檢驗費: '檢驗費',
+      結餘: '結餘',
+      就診時年齡: '就診時年齡',
+      藥費: '藥費',
+      關聯者: '關聯者',
+      看診醫師: '看診醫師',
+      單窗: '單窗',
+      主治醫師: '主治醫師',
+      處置: '處置',
+      用藥: '用藥',
+      檢驗: '檢驗'
+    }
+  }
+}
+
+export default config
---
+++ 03門診病歷/src/dts/fields-opd.d.ts
@@ -0,0 +1,122 @@
+declare namespace kintone.types {
+  interface Fields {
+    預設劑量: kintone.fieldTypes.Number;
+    分管組合搜尋: kintone.fieldTypes.SingleLineText;
+    檢驗價格3: kintone.fieldTypes.Number;
+    姓名: kintone.fieldTypes.SingleLineText;
+    檢驗價格2: kintone.fieldTypes.Number;
+    檢驗價格1: kintone.fieldTypes.Number;
+    身份別: kintone.fieldTypes.SingleLineText;
+    實收: kintone.fieldTypes.Number;
+    藥品名稱搜尋: kintone.fieldTypes.SingleLineText;
+    藥品價格3: kintone.fieldTypes.Number;
+    病歷號碼: kintone.fieldTypes.SingleLineText;
+    批價代碼: kintone.fieldTypes.Number;
+    藥品價格1: kintone.fieldTypes.Number;
+    藥品價格2: kintone.fieldTypes.Number;
+    生日: kintone.fieldTypes.Date;
+    批價狀態: kintone.fieldTypes.DropDown;
+    預設頻率: kintone.fieldTypes.SingleLineText;
+    藥品代碼搜尋: kintone.fieldTypes.SingleLineText;
+    Objective: kintone.fieldTypes.MultiLineText;
+    檢驗名稱搜尋: kintone.fieldTypes.SingleLineText;
+    批價幣別: kintone.fieldTypes.SingleLineText;
+    就診日期: kintone.fieldTypes.Date;
+    掛號費_台幣: kintone.fieldTypes.Number;
+    已領藥: kintone.fieldTypes.DropDown;
+    看診序號: kintone.fieldTypes.SingleLineText;
+    掛號流水號: kintone.fieldTypes.SingleLineText;
+    性別: kintone.fieldTypes.SingleLineText;
+    處置名稱搜尋: kintone.fieldTypes.SingleLineText;
+    檢驗代碼搜尋: kintone.fieldTypes.SingleLineText;
+    門診時段: kintone.fieldTypes.SingleLineText;
+    門診別: kintone.fieldTypes.DropDown;
+    Subjective: kintone.fieldTypes.MultiLineText;
+    預欠收: kintone.fieldTypes.Number;
+    處置價格1: kintone.fieldTypes.Number;
+    分區: kintone.fieldTypes.SingleLineText;
+    處置價格3: kintone.fieldTypes.Number;
+    就診類別: kintone.fieldTypes.SingleLineText;
+    處置價格2: kintone.fieldTypes.Number;
+    處置代碼搜尋: kintone.fieldTypes.SingleLineText;
+    療程別: kintone.fieldTypes.SingleLineText;
+    病歷記錄流水號: kintone.fieldTypes.SingleLineText;
+    身份證號: kintone.fieldTypes.SingleLineText;
+    掛號費_美金: kintone.fieldTypes.Number;
+    預設天數: kintone.fieldTypes.Number;
+    應收: kintone.fieldTypes.Calc;
+    掛號費: kintone.fieldTypes.Calc;
+    對應藥品價格: kintone.fieldTypes.Calc;
+    處置費: kintone.fieldTypes.Calc;
+    對應檢驗價格: kintone.fieldTypes.Calc;
+    對應處置價格: kintone.fieldTypes.Calc;
+    檢驗費: kintone.fieldTypes.Calc;
+    結餘: kintone.fieldTypes.Calc;
+    就診時年齡: kintone.fieldTypes.Calc;
+    藥費: kintone.fieldTypes.Calc;
+
+    關聯者: kintone.fieldTypes.UserSelect;
+    看診醫師: kintone.fieldTypes.UserSelect;
+    單窗: kintone.fieldTypes.UserSelect;
+    主治醫師: kintone.fieldTypes.UserSelect;
+    處置: {
+      type: 'SUBTABLE';
+      value: Array<{
+        id: string;
+        value: {
+          處置備註: kintone.fieldTypes.SingleLineText;
+          成數_處置: kintone.fieldTypes.Number;
+          處置名稱: kintone.fieldTypes.SingleLineText;
+          處置代碼: kintone.fieldTypes.SingleLineText;
+          數量_處置: kintone.fieldTypes.Number;
+          處置單價: kintone.fieldTypes.Number;
+          處置費小計: kintone.fieldTypes.Calc;
+        };
+      }>;
+    };
+    用藥: {
+      type: 'SUBTABLE';
+      value: Array<{
+        id: string;
+        value: {
+          藥品代碼: kintone.fieldTypes.SingleLineText;
+          藥品單價: kintone.fieldTypes.Number;
+          成數_藥品: kintone.fieldTypes.Number;
+          劑量: kintone.fieldTypes.Number;
+          天數: kintone.fieldTypes.Number;
+          藥品名稱: kintone.fieldTypes.SingleLineText;
+          總量: kintone.fieldTypes.Number;
+          用藥備註: kintone.fieldTypes.SingleLineText;
+          頻率: kintone.fieldTypes.SingleLineText;
+          藥費小計: kintone.fieldTypes.Calc;
+        };
+      }>;
+    };
+    檢驗: {
+      type: 'SUBTABLE';
+      value: Array<{
+        id: string;
+        value: {
+          檢驗備註: kintone.fieldTypes.SingleLineText;
+          檢驗單號: kintone.fieldTypes.SingleLineText;
+          成數_檢驗: kintone.fieldTypes.Number;
+          檢驗日期: kintone.fieldTypes.Date;
+          檢驗單價: kintone.fieldTypes.Number;
+          檢驗代碼: kintone.fieldTypes.SingleLineText;
+          檢驗單狀態: kintone.fieldTypes.DropDown;
+          檢驗名稱: kintone.fieldTypes.SingleLineText;
+          檢驗費小計: kintone.fieldTypes.Calc;
+        };
+      }>;
+    };
+  }
+  interface SavedFields extends Fields {
+    $id: kintone.fieldTypes.Id;
+    $revision: kintone.fieldTypes.Revision;
+    建立人: kintone.fieldTypes.Creator;
+    更新人: kintone.fieldTypes.Modifier;
+    更新時間: kintone.fieldTypes.UpdatedTime;
+    記錄號碼: kintone.fieldTypes.RecordNumber;
+    建立時間: kintone.fieldTypes.CreatedTime;
+  }
+}
